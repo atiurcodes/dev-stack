96caba8b7c3c44fa94092d369fd1cd9a496dc59a
Atiur Rahman <atiurrahman054@gmail.com>
2026-09-12 09:11:36 +0600

    footer design

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Banner from './Banner'
+import Footer from './Footer'
 import Navbar from './Navbar'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navbar />
       <Banner />
+      <Footer />
     </div>
   )
 }
